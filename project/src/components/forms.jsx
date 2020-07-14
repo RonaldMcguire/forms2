09006f4b4c4c3e3b1ce6c2930e75728a82e365d8
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-13 21:55:30 -0400

    this is where u will add breed

--- project/src/components/forms.jsx
+++ project/src/components/forms.jsx
@@ -5,42 +5,48 @@ class forms extends Component{
 
 
 
-    nameChange = (event) => {
 
-        this.setState({
-      
-      name: event.target.value
-      
-        })
-      
-      }
-    }
-    
-    BreedChange = (event) => {
-    
-      this.setState({
-        breed: event.target.value
-      })
-    }
 
-    render(){
-      return(
-        <div>
-        <h1> Puppy Palace</h1>
-        <form className = "dog-name">
-          <label>dogName</label>
-          <input
-          type = "dogName"
-          placeholder="Name of Dog"
-          value={this.state.name}
-          onChange={this.nameChange}
-    
-          />
-          </form>
-          </div>
-      )
-    }
+  nameChange = (event) => {
 
+    this.setState({
   
-    
+  name: event.target.value
+    })
+  
+  }
+}
+
+BreedChange = (event) => {
+
+  this.setState({
+    breed: event.target.value
+  })
+}
+
+render(){
+  return(
+    <div>
+    <h1> Puppy Palace</h1>
+    <form className = "dog-name">
+      <label>dogName</label>
+      <input
+      type = "dogName"
+      placeholder="Name of Dog"
+      value={this.state.name}
+      onChange={this.nameChange}
+
+      />
+            <label>Password</label>
+  <input
+  type = "Breed"
+  placeholder="Breed of Dog"
+  value={this.state.breed}
+  onChange={this.breedChange}
+  />
+      </form>
+      </div>
+  )
+}
+  
     export default forms;
