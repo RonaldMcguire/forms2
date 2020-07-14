2767edbb12e079d9230281074cf285140656fcd2
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-13 20:30:35 -0400

    test

---
+++ project/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Forms from './Component/forms'
+import './App.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <Forms />
+
+      </div>
+    )
+  }
+}
---
+++ project/src/components/forms.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class forms extends Component{
+
+
+
+
+    NameChange = (event) => {
+
+        this.setState({
+      
+      name: event.target.value
+      
+        })
+      
+      }f
+    }
+    
+    
+    export default forms;
