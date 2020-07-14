888946be63de8351054c8d3a0be43cac11b58853
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-13 22:06:14 -0400

    fixing the error in code

--- project/src/components/forms.jsx
+++ project/src/components/forms.jsx
@@ -2,51 +2,52 @@ import React, { Component } from "react";
 
 class forms extends Component{
 
+    
+    nameChange = (event) => {
+    
+      this.setState({
+    
+    name: event.target.value
+    
+      })
+    
+    }
+    
+    breedName = (event) => {
+    
+      this.setState({
+        breed: event.target.value
+      })
+    }
 
-
-
-
-
-  nameChange = (event) => {
-
-    this.setState({
-  
-  name: event.target.value
-    })
-  
-  }
-}
-
-BreedChange = (event) => {
-
-  this.setState({
-    breed: event.target.value
-  })
-}
-
-render(){
-  return(
-    <div>
-    <h1> Puppy Palace</h1>
-    <form className = "dog-name">
-      <label>dogName</label>
-      <input
-      type = "dogName"
-      placeholder="Name of Dog"
-      value={this.state.name}
-      onChange={this.nameChange}
-
-      />
-            <label>Password</label>
-  <input
-  type = "Breed"
-  placeholder="Breed of Dog"
-  value={this.state.breed}
-  onChange={this.breedChange}
-  />
-      </form>
-      </div>
-  )
-}
+    
+    
+    render(){
+      return(
+        <div>
+        <h1> Name of Dog </h1>
+        <form className = "dog-name">
+          <label>DogName</label>
+          <input
+          type = "dogName"
+          placeholder="enter name"
+          value={this.state.name}
+          onChange={this.nameChange}
+    
+          />
+          <label>Breed</label>
+          <input
+          type = "Breed"
+          placeholder="Breed"
+          value={this.state.breed}
+          onChange={this.breedName}
+          />
+        </form>
+        </div>
+      )
+    }
+    
+    }
+    
   
     export default forms;
